027deb6231ca603252f3ec11d2d5cd5b27ddf931
Onlyal33 <alex.lapygin@gmail.com>
2019-07-15 23:42:03 +0300

    project stage 4: eslint, travis-ci

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import readlineSync from 'readline-sync';
-export const greetings = () => {
+
+export default () => {
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
 };
